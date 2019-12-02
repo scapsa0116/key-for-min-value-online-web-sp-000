f4367ce0463005a1e840f710f1d9f54cae21e255
Stela Capsa <stela.capsa@gmail.com>
2019-12-02 18:04:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 require "pry"
 def key_for_min_value(name_hash)
-  binding.pry
+ 
 name_hash.collect do |a, b|
   a <=> b
+   binding.pry
   end
 end
 
